cbc0bf9c83b86ca56ed4d2ede11c3b420c219a42
s1f10240027 <s1f102400279@iniad.org>
2025-05-22 01:05:11 +0900

    新曲をアップデートできるように

--- src/update_songdata.ts
+++ src/update_songdata.ts
@@ -39,21 +39,6 @@ interface Song {
     }
 }
 
-function getSingleData(id: string) {
-    const URL_ID: string = `https://api.chunirec.net/2.0/music/show.json?region=jp2&id=${id}&token=${TOKEN}`;
-    let result: { [key: string]: number | null } = {};
-    fetch(URL_ID)
-        .then((response) => response.json())
-        .then((data: any) => {
-            result["esp"] = data.data.EXP ? data.data.EXP.const : null;
-            result["MASTER"] = data.data.MAS ? data.data.MAS.const : null;
-            result["ULTIMA"] = data.data.ULT ? data.data.ULT.const : null;
-        })
-    return result;
-}
-
-
-
 function update_songs(type: string) {
     if (type == "all") {
         fetch(URL_ALL)
@@ -133,51 +118,71 @@ function update_songs(type: string) {
             console.log(`処理完了件数: ${correct_num}`);
             console.log(`不明件数: ${Nodata_num}`);
         })();
-    } else if (type == "fix") {
-        //titleがない曲を修正するための処理
-        const URL_ALL: string = `https://api.chunirec.net/2.0/music/showall.json?region=jp2&token=${TOKEN}`;
-        const OldData = JSON.parse(fs.readFileSync("songs.json", "utf-8"));
-        const needFixSongs = OldData.filter((song: any) => !song.title);
-        fetch(URL_ALL)
+    } else if (type == "update") {
+        // songs.jsonに含まれていない新しい曲を取得して追加する
+        (async () => {     
+            const URL_ALL: string = `https://api.chunirec.net/2.0/music/showall.json?region=jp2&token=${TOKEN}`;
+            const OldData = JSON.parse(fs.readFileSync("songs.json", "utf-8"));
+
+            fetch(URL_ALL)
             .then((response) => response.json())
-            .then((data: unknown) => {
+            .then(async (data: unknown) => {
                 const songs = data as Song[];
-                needFixSongs.forEach((fixSong: any) => {
-                    const found = songs.find(song => song.meta.id === fixSong.id);
-                    if (found) {
-                        const fixedSong = {
-                            id: found.meta.id,
-                            title: found.meta.title,
-                            genre: found.meta.genre,
-                            artist: found.meta.artist,
-                            verse: fixSong.verse,
-                            dif_exp: fixSong.dif_exp,
-                            dif_mas: fixSong.dif_mas,
-                            dif_ult: fixSong.dif_ult
+                for (const song of songs) {
+                    const exists = OldData.some((existSong: any) => existSong.id === song.meta.id);
+                    if (!exists) {
+                        
+                        const URL_ID = `https://api.chunirec.net/2.0/music/show.json?region=jp2&id=${song.meta.id}&token=${TOKEN}`;
+
+                        try {
+                            const res = await fetch(URL_ID);
+                            const text = await res.text();
+                            const data = JSON.parse(text);
+                            if (!data) {
+                                continue;
+                            }
+
+                            const updatedSong = {
+                                id: song.meta.id,
+                                title: song.meta.title,
+                                genre: song.meta.genre,
+                                artist: song.meta.artist,
+                                verse: song.meta.release >= "2024-12-12",
+                                dif_exp: song.data.EXP ? song.data.EXP.const : null,
+                                dif_mas: song.data.MAS ? song.data.MAS.const : null,
+                                dif_ult: song.data.ULT ? song.data.ULT.const : null
+                            };
+
+                            OldData.push(updatedSong);
+                            console.log(`✅ 新しい曲を追加しました: ${updatedSong.title}`);
+
+                        } catch (err) {
+                            console.error(`エラー: id=${song.meta.id}`, err);
+                            break;
                         };
 
-                        const index = OldData.findIndex((db_song: any) => db_song.id === found.meta.id);
-                        if (index !== -1) {
-                            OldData[index] = fixedSong;
-                        };
-                        console.log(fixedSong);
+                        await new Promise((r) => setTimeout(r, 100));
                     };
-                });
-                fs.writeFileSync("songs.json", JSON.stringify(OldData, null, 2), "utf-8");
+                };
+                writeFileSync("songs.json", JSON.stringify(OldData, null, 2), "utf-8");
+
+            })
+            .catch((error: unknown) => {
+                console.error('Error fetching data:', error);
             });
+        })();
     }
 }
 
-rl.question("update typeを入力してください (all, single, fix): ", (input) => {
+rl.question("update typeを入力してください (all, single, update): ", (input) => {
     if (input == "all") {
         update_songs("all");
     } else if (input == "single") {
         update_songs("single");
-    } else if (input == "fix") {
-        update_songs("fix");
+    } else if (input == "update") {
+        update_songs("update");
     } else {
-        getSingleData("aa");
-        console.error("⚠️ update typeはallかsingleで入力してください。");
+        console.error("⚠️ update typeはall / single / updateで入力してください。");
     }
     rl.close();
     return;
